597038ae44d782c127115ae02e44775917e29306
Fatma3112 <105184081+Fatma3112@users.noreply.github.com>
2022-06-07 00:10:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+//date
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
+
+
+//nav
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+
+
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+
+});
+
+//fixed nav
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link")
+
+window.addEventListener("scroll", function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    if (scrollHeight > 300) {
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link");
+    }
+})
