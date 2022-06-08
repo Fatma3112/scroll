01c27cd158d54d3ff1b8420d87dac7a0e9e07ae3
Fatma3112 <105184081+Fatma3112@users.noreply.github.com>
2022-06-08 17:56:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -40,3 +40,26 @@ window.addEventListener("scroll", function() {
         topLink.classList.remove("show-link");
     }
 })
+
+
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+         window.scrollTo({
+            left: 0,
+            top: position,
+        });
+       
+    });
+});
